a38ec391e36282f05e6ba8e306add3ab9380a810
Anton Fishchev <antonfishevv@gmail.com>
2022-02-27 17:12:36 +0300

    lab 1-2

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FishchevComponent />
+  <Vyatsu :results='results'/>
+  <!-- <Autorization title='Авторизация'/> -->
+  <LoginWindow />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import FishchevComponent from 'fishchev-component';
+import Vyatsu from 'vyatsu-fishchev';
+import axios from 'axios';
+// import Autorization from 'autorization-fishchev';
+import LoginWindow from './components/LoginWindow.vue';
+
+const url = 'http://localhost:8080/admission/';
 
 export default {
-  name: 'App',
+  name: 'MainApp',
   components: {
-    HelloWorld,
+    FishchevComponent,
+    Vyatsu,
+    LoginWindow,
+  },
+  data() {
+    return { FishchevComponent, results: [] };
+  },
+  mounted() {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response);
+        this.results = response.data;
+      })
+      .catch((error) => console.log(error));
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
